b13d0f0aa00a7448a0867b308e945b7c88d6b426
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 15:21:32 +0300

    Add files via upload

---
+++ RecursionHW-Ex4/RecursionHW-Ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RecursionHW_Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(DrawTop(n));
+            Console.Write(DrawBottom(0,n));
+        }
+        static string DrawTop(int n)
+        {
+            if(n > 0)
+            {
+                string tempStr = "\n";
+                if(n == 1)
+                {
+                    tempStr = null;
+                }
+                return new string('*', n) + tempStr + DrawTop(n - 1);    
+            }
+            return null;
+        }
+        static string DrawBottom(int n, int final)
+        {
+            if(n <= final)
+            {
+                return new string('#', n) + "\n" + DrawBottom(n + 1, final);
+            }
+            return null;
+        }
+    }
+}
---
+++ RecursionHW-Ex5/RecursionHW-Ex5/RecursiveNestedLoops.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class RecursiveNestedLoops
+{
+
+    static int numberOfLoops;
+
+    static int[] loops;
+
+
+
+    static void Main()
+    {
+
+        numberOfLoops = int.Parse(Console.ReadLine());
+
+        loops = new int[numberOfLoops];
+
+        NestedLoops(0);
+
+    }
+
+
+
+    static void NestedLoops(int currentLoop)
+    {
+
+        if (currentLoop == numberOfLoops)
+
+        {
+
+            PrintLoops();
+
+            return;
+
+        }
+
+
+
+        for (int counter = 1; counter <= numberOfLoops; counter++)
+
+        {
+
+            loops[currentLoop] = counter;
+
+            NestedLoops(currentLoop + 1);
+
+        }
+
+    }
+
+
+
+    static void PrintLoops()
+    {
+
+        for (int i = 0; i < numberOfLoops; i++)
+
+        {
+
+            Console.Write("{0} ", loops[i]);
+
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
